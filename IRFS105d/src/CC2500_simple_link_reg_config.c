d2f990740341dcd22827a1a321e69e9468123143
hylics <aethylic@gmail.com>
2015-08-19 17:46:03 +0300

    test
    d4715 not work

--- IRFS105d/src/CC2500_simple_link_reg_config.c
+++ IRFS105d/src/CC2500_simple_link_reg_config.c
@@ -33,6 +33,7 @@ B4700 D0    A7
 D4713 D0    B8
 D4702 D0    7D
 D4714 CF    A2
+D4715 3E    0D
 D4716 D0    C5
 */
 
@@ -40,9 +41,9 @@ const uint8_t preferredSettings[][2]=
 {
   {CC2500_FSCTRL1,      0x06},
   {CC2500_FSCTRL0,      0x00},
-  {CC2500_FREQ2,        0x59},
-  {CC2500_FREQ1,        0xD0}, //b4715 CF
-  {CC2500_FREQ0,        0xC5}, //  89
+  {CC2500_FREQ2,        0x5B},
+  {CC2500_FREQ1,        0x3E}, //b4715 CF
+  {CC2500_FREQ0,        0x0D}, //  89
   {CC2500_MDMCFG4,      0x5B},
   {CC2500_MDMCFG3,      0xE5},
   {CC2500_MDMCFG2,      0x73},
